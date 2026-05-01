3a8392a99b86cd859f9a838923d92f262d97fb46
Matheus Ribeiro <mathribemathribe@gmail.com>
2026-05-01 15:15:13 -0300

    feat: containeriza DevWatch com Docker e PostgreSQL
    * adiciona Dockerfile da API FastAPI
    * configura docker-compose para API + PostgreSQL
    * adiciona variáveis de ambiente com .env
    * configura persistência do banco com volumes
    * ajusta conexão SQLAlchemy para ambiente Docker
    * implementa comunicação entre containers
    * permite subir o projeto com docker compose up --build
    * profissionaliza estrutura inicial do projeto

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.12
+FROM python:3.12-slim
 
 WORKDIR /app
 
@@ -7,5 +7,7 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+
+EXPOSE 8000
 
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/database.py
+++ app/database.py
@@ -1,15 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config import DATABASE_URL
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-DATABASE_URL = f"postgresql+psycopg2://postgres:8605@{DB_HOST}/devwatch"
-
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(DATABASE_URL)
 engine = create_engine(
     DATABASE_URL
 )
