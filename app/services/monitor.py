dca1e82d02cb7de7ff4d437e93f5b4fe15bf1c02
Matheus Ribeiro <mathribemathribe@gmail.com>
2026-05-02 15:42:06 -0300

    feat: adiciona coleta contínua de CPU, RAM e disco

--- app/services/monitor.py
+++ app/services/monitor.py
@@ -1,11 +1,31 @@
 import time
 import threading
 import psutil
+import os
+
+disk_path = "C:\\" if os.name == "nt" else "/"
 
 def monitor_loop():
+    
     while True:
-        #parei aqui----------------------
+        try:
+            cpu_percent = psutil.cpu_percent()
+            disk_percent = psutil.disk_usage(disk_path).percent
+            memory_percent = psutil.virtual_memory().percent
+
+            print(f"""
+            CPU:{cpu_percent}%
+            RAM:{memory_percent}%
+            DISK:{disk_percent}%
+""",flush=True)
+           
+        
+        except Exception as e:
+            print(e)
+
         time.sleep(5)
+
+        
 
 def start_monitor():
     thread = threading.Thread(target=monitor_loop, daemon=True)
